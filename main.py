77ecbc733494d70cad0eabc050c422081490d74c
mahajialirezaei <m.a.hajialirezaei05@gmail.com>
2025-03-22 22:26:54 +0330

    define x, y test, train

--- main.py
+++ main.py
@@ -11,3 +11,10 @@ CATEGORICAL_COLS = ['Sex', 'Pclass', 'Embarked', 'SibSp', 'Parch']
 NUMERIC_COLS = ['Age', 'Parch']
 
 tit_df.dropna(subset=['Survive', 'Age', 'Fare', 'Embarked'], inplace=True)
+
+y = tit_df['Survive']
+x = pd.get_dummies(tit_df[CATEGORICAL_COLS + NUMERIC_COLS])
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=63)
+
