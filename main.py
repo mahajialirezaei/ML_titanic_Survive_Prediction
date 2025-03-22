9efadac675df812d2cb74ff92f89cba5a02d4b80
mahajialirezaei <m.a.hajialirezaei05@gmail.com>
2025-03-22 22:19:47 +0330

    read table

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+
+
+tit_df = pd.read_csv('titanic.csv')
